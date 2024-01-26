b41b114f78387b04d048ad60289316db4386ebf1
Anthony Capobianco <vt_@gmx.us>
2024-01-26 07:56:10 +0100

    Added sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,13 @@
+<?php
+  $ihatephp = new SimpleXMLElement(<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+  <loc>https://anth.pro/</loc>
+  <lastmod></lastmod>
+</url>
+</urlset>
+XML);
+  $ihatephp->url->lastmod = date(DATE_ATOM);
+  echo $ihatephp->asXML();
+?>
